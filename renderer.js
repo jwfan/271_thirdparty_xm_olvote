2c47358a31834c717eed4c85d7aa86f7cdfe0dc5
Zehuan Li <zhli@jhu.edu>
2018-03-25 19:03:30 -0700

    Single vote completed

--- renderer.js
+++ renderer.js
@@ -10,6 +10,11 @@ holder.ondragleave = holder.ondragend = function () {
  return false;
 };
 
+document.getElementById('codeinput').addEventListener("keydown", function(event) {
+  if (event.which == 13 || event.keyCode == 13)
+    submit(this.value);
+});
+
 holder.ondrop = function (e) {
   e.preventDefault();
   var file = e.dataTransfer.files[0];
@@ -22,23 +27,75 @@ holder.ondrop = function (e) {
   return false;
 };
 
-
 function preprocess(line){
   return {username: line.split(' ')[0].split('.')[1], password: line.split(' ')[1]};
 }
 
+CAPTCODE_URL = 'https://account.xiaomi.com/pass/getCode?icodeType=login&'
+FIRST_URL = 'https://passport.iqiyi.com/apis/thirdparty/login.action?type=30&isiframe=1&ppsNew=1&agenttype=39&isapp=0&pps_use_https_redirect=true&use_https_redirect=true&url=https://game.iqiyi.com/weblogin/login?preurl=http://www.iqiyi.com/common/idol_vote.html'
+VOTE_URL = 'http://vote.i.iqiyi.com/eagle/outer/join_common_vote?vid=1251575812010177&options=%7B%221218905683020731%22%3A%5B1487907121030964%5D%7D&appid=6&authCookie='
+
+var request = require('request');
+var jar = request.jar();
+
 function vote(data){
-  var un = data.username;
-  var pw = data.password;
-  var request = require('request');
-  request.post({url:'https://account.xiaomi.com/pass/serviceLoginAuth2?_dc=' + (new Date).getTime(), form: {user: '+86' + un, hash: hash(pw)}}, function(err,httpResponse,body){
-    console.log(err);
-    console.log(httpResponse);
-    if (httpResponse == 302)
-      console.log(body);
+  un = data.username;
+  pw = data.password;
+  request.get({url: FIRST_URL, jar: jar, followRedirect: false}, function(err, res, body) {
+    url_2 = res.headers.location;
+    request.get({url: url_2, jar: jar, followRedirect: false}, function(err, res, body) {
+      url_3 = res.headers.location;
+      var code_request = require('request').defaults({ encoding: null });
+      var timestamp = (new Date).getTime();
+      var code_url = CAPTCODE_URL + timestamp;
+      code_request.get({url: code_url, jar: jar, followRedirect: false}, function(err, res, body) {
+        document.getElementById('codeimg').src = 'data:' + res.headers['content-type'] + ';base64,' + new Buffer(body).toString('base64');
+      });
+    });
   });
 }
 
-function hash(pw){
-  return pw;
+function submit(captcha) {
+  request.get({url: url_3, jar: jar, followRedirect: false}, function(err, res, body) {
+    callback = extract(body,'callback', 'JSP_VAR');
+    sid = extract(body,'sid:', 'JSP_VAR');
+    qs = extract(body,'qs', 'JSP_VAR');
+    _sign = extract(body,'"_sign"', 'JSP_VAR');
+    serviceParam = extract(body,'serviceParam', 'JSP_VAR', '\'');
+    post_param = {_json: true, callback: callback, sid: sid, qs: qs, _sign: _sign, serviceParam: serviceParam, captCode: captcha, user: '+86' + un, hash: hash(pw)};
+    ick = jar.getCookies(CAPTCODE_URL)[0].value;
+    request.post({url:'https://account.xiaomi.com/pass/serviceLoginAuth2?_dc=' + (new Date).getTime(), form: post_param, jar: jar, followRedirect: false}, function(err, res, body){
+      body_json = JSON.parse(body.substring(body.indexOf('{')));
+      url_4 = body_json.location;
+      request.get({url: url_4, jar: jar, followRedirect: false}, function(err, res, body) {
+        url_5 = res.headers.location;
+        request.get({url: url_5, jar: jar, followRedirect: false}, function(err, res, body) {
+          url_6 = res.headers.location;
+          request.get({url: url_6, jar: jar, followRedirect: false}, function(err, res, body) {
+            final_url = res.headers.location;
+            ac_name = 'authcookie=';
+            ac_start = final_url.indexOf(ac_name) + ac_name.length;
+            ac_end = final_url.indexOf('&', ac_start);
+            authcookie = final_url.substring(ac_start, ac_end);
+            request.get({url: VOTE_URL + authcookie, jar: jar, followRedirect: false}, function(err, res, body) {
+              console.log(JSON.parse(body).msg);
+            });
+          });
+        });
+      });
+    });
+  });
+}
+
+function hash(pw) {
+  var md5 = require('md5');
+  return md5(pw).toUpperCase();
+}
+
+function extract(text, key, pre_key, sep='"') {
+  pre_index = text.indexOf(pre_key);
+  start_index =  text.indexOf(key, pre_index) + key.length;
+  end_index = text.indexOf('\n', start_index);
+  raw =  text.substring(start_index, end_index);
+  return raw.substring(raw.indexOf(sep) + 1, raw.lastIndexOf(sep));
 }
